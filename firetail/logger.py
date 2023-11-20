3f5580fb719a3014b30aff5f8aaa65c850607341
theteacat <joshua@firetail.io>
2023-11-20 14:19:13 +0000

    Remove dead code & simplify logger

--- firetail/logger.py
+++ firetail/logger.py
@@ -2,28 +2,23 @@ import logging
 import sys
 from threading import Lock
 
-logger_names_with_handlers_lock = Lock()
-logger_names_with_handlers = set()
-
-
-def get_logger(debug):
-    return __get_logger(debug, __name__)
+configured_loggers_lock = Lock()
+configured_loggers = set()
 
 
 def get_stdout_logger(debug):
-    stdout_logger = __get_logger(debug, __name__ + "_stdout", logging.StreamHandler(sys.stdout))
-    stdout_logger.propagate = False
+    stdout_logger_name = __name__ + "_stdout"
+    stdout_logger = logging.getLogger(stdout_logger_name)
+
+    requires_config = False
+    with configured_loggers_lock:
+        if stdout_logger_name not in configured_loggers:
+            configured_loggers.add(stdout_logger_name)
+            requires_config = True
+
+    if requires_config:
+        stdout_logger.propagate = False
+        stdout_logger.setLevel(logging.DEBUG if debug else logging.INFO)
+        stdout_logger.addHandler(logging.StreamHandler(sys.stdout))
+
     return stdout_logger
-
-
-def __get_logger(debug, name, handler=None):
-    logger = logging.getLogger(name)
-
-    with logger_names_with_handlers_lock:
-        if name not in logger_names_with_handlers:
-            logger.setLevel(logging.DEBUG if debug else logging.INFO)
-            if handler:
-                logger.addHandler(handler)
-            logger_names_with_handlers.add(name)
-
-    return logger
